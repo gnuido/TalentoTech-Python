a89df6df731769faee49eec151da88d66134ae9a
gnuido <gnuido@protonmail.com>
2025-02-12 21:26:32 -0300

    completado opción de impresión de productos

--- proyectoIntegrador.py
+++ proyectoIntegrador.py
@@ -5,7 +5,8 @@ import sqlite3
 #%--------------------------------------
 def menuOpciones():
 	
-	menuPrincipal	=	['#################################################\n|'
+	menuPrincipal	=	['\n\n#################################################\n'
+						'|\t\t\t\t\t\t|\n'
 						'|Seleccione una de las siguientes opciones:	|\n'
 						'|\t\t\t\t\t\t|\n'
 						'|1> Ver inventario.\t\t\t\t|',
@@ -15,13 +16,15 @@ def menuOpciones():
 						'|5> Buscar producto en el inventario.\t\t|',
 						'|6> Ayuda / FAQ\t\t\t\t\t|',
 						'|7> Salir.\t\t\t\t\t|\n'
-						'#################################################\n']
+						'|\t\t\t\t\t\t|\n'
+						'#################################################'			]
 #solo las opciones suman a la cantidad de elementos en lista, que se emplea en controlOpciones()
-
+	
 	for i in menuPrincipal:
 		print(i)
-	
+		
 	opcion = controlOpciones(menuPrincipal)
+	return(opcion)
 #-------------------
 def controlOpciones(menuActual):	
 	#control de ingreso válido
@@ -34,11 +37,11 @@ def controlOpciones(menuActual):
 				return(opcion)
 				break
 			else:
-				print('La opción seleccionada no es válida, por favor, ingrese el dígito correspondiente a la opción escogida: ')
+				print('\nLa opción seleccionada no es válida, por favor, ingrese el dígito correspondiente a la opción escogida: \n')
 				continue
 		
 		except ValueError:
-			print('Debe ingresar una opción válida: ')
+			print('\nDebe ingresar una opción válida: \n')
 			continue
 			
 #-------------------			
@@ -48,34 +51,36 @@ def crearTabla():
 	cursor = conexion.cursor()
 	
 	cursor.execute("""CREATE TABLE IF NOT EXISTS PRODUCTOS (
-	'ID' PRIMARY KEY NOT NULL UNIQUE AUTOINCREMENT,
+	'ID' NOT NULL UNIQUE,
 	'MARCA' TEXT NOT NULL,
 	'DESCRIPCION' TEXT NOT NULL,
 	'PRECIO' REAL,
 	'STOCK' INTEGER NOT NULL,
 	'UBICACION' INTEGER,
-	'PROVEEDOR' TEXT)
+	'PROVEEDOR' TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT))
 	""")
 	
 	conexion.close()
 	
 #-------------------
-def mostrarInventario(opcionSubMenu):
+def mostrarProductos():
 	
-	mostrarSubMenu =	['1> Mostrar inventario completo.\n',
+	#menù
+	mostrarSubMenu =	['\n1> Mostrar inventario completo.\n',
 						 '2> Mostar productos fuera de stock.\n',
 						 '3> Mostar productos sin ubicar.\n',
 						 '4> Mostrar productos sin proveedores.\n',
-						 '5> Mostar productos sin precio.']
+						 '5> Mostar productos sin precio.'			]
 	
 	for i in mostrarSubMenu:
 		print(i)
 	
-	opcionSubMenu = controlOpciones(mostrarSubMenu)
-	
 	conexion = sqlite3.connect("INVENTARIO.db")
 	cursor = conexion.cursor()
 	
+	#opciones de flujo
+	opcionSubMenu = controlOpciones(mostrarSubMenu)
 	if opcionSubMenu == 1:
 		query = "SELECT * FROM PRODUCTOS"
 		cursor.execute(query)
@@ -99,31 +104,43 @@ def mostrarInventario(opcionSubMenu):
 	elif opcionSubMenu == 5:
 		query = "SELECT * FROM PRODUCTOS WHERE PRECIO = NULL"
 		cursor.execute(query)
-		inventario = cursor.fetchall()
+		inventario = cursor.fetchall(5)
 		
+	#impresión de datos en egreso
 	if len(inventario) == 0:
-		print('No hay productos que mostrar.')
+		print('\nNo hay productos que mostrar.\n')
+	
 	else:
-		print(inventario)
-		
+		print('\n%------------------------------------------------%\n')
+		for n in range(len(inventario)):
+			print(	'ID: ' + str(inventario[n][0]),
+					'\nMarca: ' + str(inventario[n][1]),
+					'\nDescripción: ' + str(inventario[n][2]),
+					'\nPrecio: ' + str(inventario[n][3]),
+					'\nStock: ' + str(inventario[n][4]),
+					'\nUbicación: ' + str(inventario[n][5]),
+					'\nProveedor: ' + str(inventario[n][6]),	
+					'\n'										)
+		print('%------------------------------------------------%')
+	
 	conexion.close()
 
 #-------------------
-def añadirInventario():
-	conexion = sqlite3.connect('INVENTARIO.db')
+def añadirProductos():
+	conexion = sqlite3.connect("INVENTARIO.db")
 	cursor = conexion.cursor()
 	
-	query = "INSERT "
-
-#-------------------
-def buscarInventario():
-	conexion = sqlite3.connect('INVENTARIO.db')
-	cursor = conexion.cursor()
+	query = ("""INSERT INTO PRODUCTOS ('MARCA', 'DESCRIPCION', 'PRECIO', 'STOCK', 'UBICACION', 'PROVEEDORES'),
+	VALUES ()""")
 	
-	query = "SELECT "
 #%--------------------------------------
+#inicio
+crearTabla()
 
+while True:
+	opcion = menuOpciones()
 
-opcion = menuOpciones()
-
-
+	#flujo
+	if opcion == 1:
+		mostrarProductos()
+		continue
